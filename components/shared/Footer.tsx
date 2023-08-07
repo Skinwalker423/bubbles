49e1b2efb083fea1a4ab0e8552e70f5e08431f5c
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-06 19:57:17 -0700

    footer styling

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const path = usePathname();
+  const router = useRouter();
+  return (
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map(({ imgURL, label, route }) => {
+          const isActive =
+            route === path ||
+            (path.includes(route) && route.length > 1);
+          return (
+            <Link
+              key={label}
+              className={`bottombar_link group relative ${
+                isActive && "bg-primary-500"
+              }`}
+              href={route}
+            >
+              <Image
+                src={imgURL}
+                alt={label}
+                width={24}
+                height={2}
+              />
+
+              <p className='text-light-1 max-lg:hidden'>
+                {label}
+              </p>
+              <span
+                className='lg:hidden group-hover:opacity-100 transition-opacity bg-light-1 px-1 text-[12px] text-dark-1 rounded-md absolute bottom-14 left-1/2 
+                -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto truncate'
+              >
+                {label}
+              </span>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
