1c69a7e5552543f1f1bba1afae7f924b8873ff7a
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-07 15:19:52 -0700

    onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -3,10 +3,13 @@ import { UserButton } from "@clerk/nextjs";
 
 const Onboarding = () => {
   return (
-    <div className='text-white'>
-      Onboarding
+    <main className='mx-auto max-w-3xl flex flex-col justify-start px-10 py-20 border '>
+      <h1 className='head-text'>Onboarding</h1>
+      <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile to use Bubbles
+      </p>
       <UserButton />
-    </div>
+    </main>
   );
 };
 
