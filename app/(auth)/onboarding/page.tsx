1b783492218510099218b9e72aff40ad7b327956
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-07 15:32:49 -0700

    userData

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
 
-const Onboarding = () => {
+const Onboarding = async () => {
+  const user = await currentUser();
+
+  const userInfo = {
+    _id: "",
+    username: "",
+    name: "",
+    bio: "",
+    image: "",
+  };
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: user?.username || userInfo.username,
+    name: user?.lastName || userInfo.name || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
   return (
     <main className='mx-auto max-w-3xl flex flex-col justify-start px-10 py-20 border '>
       <h1 className='head-text'>Onboarding</h1>
       <p className='mt-3 text-base-regular text-light-2'>
         Complete your profile to use Bubbles
       </p>
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile />
+      </section>
       <UserButton />
     </main>
   );
