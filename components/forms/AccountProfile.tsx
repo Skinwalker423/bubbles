7f244f15fb532bce82914266b723c6f0a92c6be7
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-07 17:19:24 -0700

    input component

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import React from "react";
 import { UserDataProps } from "@/app/(auth)/onboarding/page";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
 
 interface AccountProfileProps {
   user: UserDataProps;
@@ -10,6 +25,15 @@ const AccountProfile = ({
   user,
   btnTitle,
 }: AccountProfileProps) => {
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
   return <div>AccountProfile</div>;
 };
 
