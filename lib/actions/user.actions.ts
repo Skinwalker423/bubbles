e8fd7ebdf359d061ed3b5351bccac1c51daaae18
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-10 15:53:51 -0700

    user schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,1 +1,7 @@
 "use server";
+
+import { connectToMongoDb } from "../mongoose";
+
+export const updateUser = async (): Promise<void> => {
+  connectToMongoDb();
+};
---
+++ lib/models/user.model.ts
@@ -0,0 +1,39 @@
+import { Email } from "@clerk/nextjs/server";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
+});
+
+export const User = mongoose.model("User", UserSchema);
