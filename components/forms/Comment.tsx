4c5849f4d1b68a0341b7bd9ce376907fcef156c1
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-14 21:10:19 -0700

    comment form

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React, { FormEvent } from "react";
-import { UserDataProps } from "@/app/(auth)/onboarding/page";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import * as z from "zod";
-import { Textarea } from "@/components/ui/textarea";
 import {
   Form,
   FormControl,
@@ -28,11 +26,13 @@ import Image from "next/image";
 interface CommentProps {
   bubbleId: string;
   currentUserImg: string;
+  currentUserId: string;
 }
 
 const Comment = ({
   bubbleId,
   currentUserImg,
+  currentUserId,
 }: CommentProps) => {
   const router = useRouter();
   const pathname = usePathname();
@@ -40,7 +40,7 @@ const Comment = ({
   const form = useForm({
     resolver: zodResolver(CommentValidation),
     defaultValues: {
-      comment: "",
+      bubble: "",
     },
   });
 
@@ -52,40 +52,39 @@ const Comment = ({
 
   return (
     <Form {...form}>
-      <h2 className='text-light-2'>Comment</h2>
       <form
-        className='flex justify-between items-center gap-10 max-w-3xl mt-10'
+        className='flex justify-between items-center gap-5 w-full mt-10 border-t-2 border-b-2 py-7 border-y-dark-2'
         onSubmit={form.handleSubmit(onSubmit)}
       >
         <FormField
           control={form.control}
-          name='comment'
+          name='bubble'
           render={({ field }) => (
-            <FormItem className='flex items-center justify-between w-full gap-3'>
-              <FormLabel className='text-base-semibold text-light-2'>
+            <FormItem className='flex items-center w-full gap-3'>
+              <FormLabel>
                 <Image
-                  src={currentUserImg}
-                  width={24}
-                  height={24}
+                  src={currentUserImg || "/assets/user.svg"}
+                  width={48}
+                  height={48}
                   alt={"user avatar"}
+                  className={`rounded-full object-cover ${
+                    !currentUserImg && "border"
+                  }`}
                 />
               </FormLabel>
-              <FormControl>
-                <Textarea
-                  className='account-form_input no-focus'
-                  rows={1}
+              <FormControl className='border-none bg-transparent'>
+                <Input
+                  type='text'
+                  className=' text-light-1 no-focus outline-none'
+                  placeholder='Comment...'
                   {...field}
                 />
               </FormControl>
-              <FormMessage />
             </FormItem>
           )}
         />
-        <Button
-          type='submit'
-          className='bg-primary-500 rounded-full'
-        >
-          Comment
+        <Button type='submit' className='comment-form_btn'>
+          Reply
         </Button>
       </form>
     </Form>
--- lib/validations/bubble.ts
+++ lib/validations/bubble.ts
@@ -8,7 +8,7 @@ export const BubbleValidation = z.object({
   accountId: z.string().nonempty(),
 });
 export const CommentValidation = z.object({
-  comment: z
+  bubble: z
     .string()
     .nonempty()
     .min(3, { message: "Minimum 3 characters" }),
