516255cf2b94ed23fdcd2db924756f563f3acac8
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-16 16:55:39 -0700

    comments on profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,9 @@
+import BubbleCard from "@/components/cards/BubbleCard";
+import User from "@/lib/models/user.model";
+import { connectToMongoDb } from "@/lib/mongoose";
+import { currentUser } from "@clerk/nextjs";
 import React from "react";
+import { CommentProps } from "@/lib/types";
 
 interface ProfileProps {
   params: {
@@ -6,12 +11,57 @@ interface ProfileProps {
   };
 }
 
-const Profile = ({ params }: ProfileProps) => {
+const Profile = async ({ params }: ProfileProps) => {
   console.log(params.id);
+  connectToMongoDb();
+  const user = await currentUser();
+  const userProfile = await User.findOne({
+    id: params.id,
+  }).populate({
+    path: "bubbles",
+    model: "Bubble",
+    populate: {
+      path: "author",
+      model: "User",
+    },
+  });
+
+  if (!userProfile) {
+    throw new Error("no user found");
+  }
+
+  console.log(userProfile.bubbles);
+  const bubblesList = userProfile.bubbles.map(
+    (bubble: CommentProps) => {
+      return (
+        <BubbleCard
+          key={bubble._id}
+          id={bubble._id}
+          currentUserId={user?.id || ""}
+          content={bubble.text}
+          community={bubble.community}
+          author={bubble.author}
+          createdAt={bubble.createdAt}
+          comments={bubble.children}
+          parentId={bubble.parentId || ""}
+          isComment={false}
+        />
+      );
+    }
+  );
+
+  console.log(bubblesList);
+
   return (
-    <div>
-      <h1 className='text-light-1'>Profile for...</h1>
-    </div>
+    <section>
+      <h1 className='text-light-1'>
+        Profile for {userProfile.username}
+      </h1>
+      <p className='text-light-1'>{userProfile.bio}</p>
+      <div className='flex flex-col gap-5'>
+        {userProfile.bubbles.length > 0 && bubblesList}
+      </div>
+    </section>
   );
 };
 
