9410ee87f37a64e017173d2f8adbebfb9b4b17ea
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-18 16:09:09 -0700

    fetchUserPosts

---
+++ components/shared/BubblesTabs.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface BubblesTabsProps {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+const BubblesTabs = ({
+  currentUserId,
+  accountId,
+  accountType,
+}: BubblesTabsProps) => {
+  return <div>BubblesTabs</div>;
+};
+
+export default BubblesTabs;
