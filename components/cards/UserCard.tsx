f193432fdcfb97f4cce20dc116142ff1add939bf
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-20 21:48:10 -0700

    user card routing to profile

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import React from "react";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 interface UserCardProps {
   id: string;
@@ -15,7 +18,32 @@ const UserCard = ({
   personType,
   username,
 }: UserCardProps) => {
-  return <div>UserCard</div>;
+  return (
+    <article className='user-card w-full bg-dark-4 px-4 py-2 rounded-lg'>
+      <div className='user-card_avatar'>
+        <Image
+          src={imgUrl}
+          width={48}
+          height={48}
+          className='rounded-full'
+          alt={`avatar image for ${username}`}
+        />
+        <div className='flex-1 text-ellipsis'>
+          <h4 className='text-base-semibold text-light-1'>
+            {name}
+          </h4>
+          <p className='text-small-medium text-gray-1'>
+            {username}
+          </p>
+        </div>
+      </div>
+      <Link href={`/profile/${id}`}>
+        <Button className='user-card_btn'>
+          View Profile
+        </Button>
+      </Link>
+    </article>
+  );
 };
 
 export default UserCard;
