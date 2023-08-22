298588340fe0465a9ff9ffb9eb6e07e2308f4c76
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-21 21:52:37 -0700

    activity card

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -4,7 +4,8 @@ import {
   getActivity,
 } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
-import BubbleCard from "@/components/cards/BubbleCard";
+import Link from "next/link";
+import Image from "next/image";
 
 const Activity = async () => {
   const { user, userProfile } =
@@ -14,30 +15,42 @@ const Activity = async () => {
 
   const replies = await getActivity(userProfile._id);
 
-  const activityList = replies.map((bubble) => {
+  const activityList = replies.map((comment) => {
     const isBubbleLiked: boolean =
-      userProfile?.likes.includes(bubble._id.toString());
+      userProfile?.likes.includes(comment._id.toString());
     return (
-      <BubbleCard
-        key={bubble._id}
-        id={bubble._id}
-        currentUserId={user?.id || ""}
-        content={bubble.text}
-        community={bubble.community}
-        author={bubble.author}
-        createdAt={bubble.createdAt}
-        comments={bubble.children}
-        parentId={bubble.parentId || ""}
-        isComment={false}
-        liked={isBubbleLiked}
-      />
+      <Link href={`/bubble/${comment.parentId}`}>
+        <article className='activity-card'>
+          <Image
+            src={comment.author.image}
+            alt={`profile image of ${comment.author.name}`}
+            width={20}
+            height={20}
+            className='object-cover rounded-full'
+          />
+          <p className='!text-small-regular text-light-1'>
+            <span className='mr-1 text-primary-500'>
+              {comment.author.name}
+            </span>{" "}
+            replied to your bubble
+          </p>
+        </article>
+      </Link>
     );
   });
 
   return (
     <section>
       <h1 className='text-light-1'>Activity</h1>;
-      <li>{activityList}</li>
+      <section className='mt-10 flex flex-col gap-5'>
+        {activityList.length > 0 ? (
+          activityList
+        ) : (
+          <p className='!text-base-regular text-light-3'>
+            No Recent Activity
+          </p>
+        )}
+      </section>
     </section>
   );
 };
