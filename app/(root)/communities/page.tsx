530999c7ab860abb1bad2a8afed11a4178c2a771
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-22 21:55:26 -0700

    fetch communities

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,7 +1,44 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import Image from "next/image";
 import React from "react";
+import { fetchCurrentUserAndUserProfile } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>page</div>;
+const Page = async () => {
+  const { user, userProfile } =
+    await fetchCurrentUserAndUserProfile();
+  if (!user) return null;
+  if (!userProfile.onboarded) redirect("/onboarding");
+
+  const communitiesList = userProfile.communities.map(
+    (community: any) => {
+      return (
+        <div>
+          <h3>{community._id.toString()}</h3>
+        </div>
+      );
+    }
+  );
+
+  return (
+    <section>
+      <ProfileHeader
+        accountId={userProfile.id}
+        authUserId={user.id}
+        bio={userProfile.bio}
+        imgUrl={userProfile.image}
+        name={userProfile.name}
+        username={userProfile.username}
+      />
+      {communitiesList.length > 0 ? (
+        <p className=' text-light-2'>{communitiesList}</p>
+      ) : (
+        <p className=' text-light-2'>
+          No communities added
+        </p>
+      )}
+    </section>
+  );
 };
 
-export default page;
+export default Page;
